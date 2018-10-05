733a6629e4d047a2047fbfc7739ef376656f9122
Radek Los <radek.los@gmail.com>
2018-10-05 16:54:04 +0200

    Return converted html as base64 string in http response

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var MemoryStream = require('memorystream');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
+    var body = JSON.parse(event.body);
 	var memStream = new MemoryStream();
-	var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
-	wkhtmltopdf(html_utf8, event.options, function(code, signal) { context.done(null, { pdf_base64: memStream.read().toString('base64') }); }).pipe(memStream);	
+	var html_utf8 = new Buffer(body.html_base64, 'base64').toString('utf8');
+	wkhtmltopdf(html_utf8, body.options, function(code, signal) {
+	    const response = {
+            statusCode: 200,
+            body: JSON.stringify({ pdf_base64: memStream.read().toString('base64') })
+        };
+        callback(null, response);
+	}).pipe(memStream);
 };
