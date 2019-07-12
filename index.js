bb193480df7dcc52cf073ac68809152146293f92
levipro <47224088+levipro@users.noreply.github.com>
2019-07-11 21:10:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var MemoryStream = require('memorystream');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 exports.handler = function(event, context, callback) {
-    var body = JSON.parse(event.body);
+    var body = event;
 	var memStream = new MemoryStream();
     var content;
 	if (body.htmlBase64 != null) {
@@ -15,10 +15,7 @@ exports.handler = function(event, context, callback) {
 	wkhtmltopdf(content, body.options, function(code, signal) {
 	    const response = {
             statusCode: 200,
-            body: JSON.stringify({
-                pdfBase64: memStream.read().toString('base64'),
-                options: body.options
-            })
+            pdfBase64: memStream.read().toString('base64')
         };
         callback(null, response);
 	}).pipe(memStream);
